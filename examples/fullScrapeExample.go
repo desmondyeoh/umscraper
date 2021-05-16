66498bfc9b54378c656587fa8c9647ca1f19d504
Desmond Yeoh Teik Boon <desmond.yeohtb@shopee.com>
2021-05-16 18:23:42 +0800

    fix example

--- examples/fullScrapeExample.go
+++ examples/fullScrapeExample.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"time"
+
+	"github.com/desmondyeoh/umscraper"
 )
 
 // This example shows how to scrape the data fully.
@@ -14,15 +16,15 @@ func main() {
 	sleepBetweenPtjJab := 1000 * time.Millisecond
 
 	// Stage 0 - Create a data directory to store scraped data
-	scraper.CreateDirectoryIfNotExist("data", 0700)
+	umscraper.CreateDirectoryIfNotExist("data", 0700)
 
 	// Stage 1 - Scrape full list of (ptjCode, ptjText, jabCode, jabText)
-	ptjJabTable := scraper.ScrapePtjJabTable(sleepBetweenPtj)
-	scraper.WriteCsv(ptjJabTable, "data/ptjJabTable.csv")
+	ptjJabTable := umscraper.ScrapePtjJabTable(sleepBetweenPtj)
+	umscraper.WriteCsv(ptjJabTable, "data/ptjJabTable.csv")
 
 	// Stage 2 - Scrape full list of (ptjCode, ptjText, jabCode, jabText, staffName, staffDetails...)
-	ptjJabTable = scraper.ReadCsv("data/ptjJabTable.csv")
-	staffTable := scraper.ScrapeStaffTable(ptjJabTable, sleepBetweenPtjJab)
-	scraper.WriteCsv(staffTable, "data/staffTable.csv")
+	ptjJabTable = umscraper.ReadCsv("data/ptjJabTable.csv")
+	staffTable := umscraper.ScrapeStaffTable(ptjJabTable, sleepBetweenPtjJab)
+	umscraper.WriteCsv(staffTable, "data/staffTable.csv")
 
 }
